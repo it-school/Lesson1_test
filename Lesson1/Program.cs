8aa30afe3976f95b3fc284da80171d7e348f221f
it-school <it@osvita.od.ua>
2019-02-23 18:18:13 +0200

    Добавьте файлы проекта.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static int a = 1;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("1afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            Console.WriteLine("2afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            a++;
+            Console.WriteLine("3afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            Console.WriteLine("4afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            Console.WriteLine("5afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            a++;
+            Console.WriteLine("6afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            Console.WriteLine("7afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+            Console.WriteLine("8afsgdxhgcjhvkhjlkbjbhjkghjfghdfgsdf");
+
+
+        }
+    }
+}
