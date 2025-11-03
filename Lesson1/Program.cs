f6ccd2f1a9e2a787101dc17b619e343a30ff975b
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2025-11-03 22:12:52 +0200

    Enhance console output with color formatting
    Added console color changes text output.

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -7,6 +7,11 @@ namespace Lesson1111
     {
         static void Main(string[] args)
         {            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Мое имя: ...");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Мой город: ...");
+            Console.ResetColor(); // Не забудьте сбросить цвет в конце!
             Console.WriteLine();
             Console.WriteLine("8 messages will be printed line by line: ");
             Console.WriteLine("1 line of text");
